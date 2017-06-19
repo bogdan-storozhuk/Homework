6f42092e1eeeebf53f2791247c9727da4423e687
Bogdan <you@example.com>
2017-06-19 22:20:01 +0300

    added task2_1, task2_2

---
+++ task2_1/WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow 
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ListBoxTypeSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedItem = (type.SelectedItem as ListBoxItem);
+            switch (selectedItem.Content.ToString())
+            {
+                case "INT":
+                    ShowIntValue();
+                    break;
+                case "LONG":
+                    ShowLongValue();
+                    break;
+                case "FLOAT":
+                    ShowFloatValue();
+                    break;
+                case "DOUBLE":
+                    ShowDoubleValue();
+                    break;
+                case "DECIMAL":
+                    ShowDecimalValue();
+                    break;
+                case "STRING":
+                    ShowStringValue();
+                    break;
+                case "CHAR":
+                    ShowCharValue();
+                    break;
+                case "BOOL":
+                    ShowBoolValue();
+                    break;
+            }
+
+        }
+        private void ShowIntValue()
+        {
+            value.Text = int.MaxValue.ToString();
+            value1.Text = int.MinValue.ToString();
+            value3.Text = default(int).ToString();
+        }
+
+        private void ShowLongValue()
+        {
+            value.Text = long.MaxValue.ToString();
+            value1.Text = long.MinValue.ToString();
+            value3.Text = default(long).ToString();
+        }
+
+        private void ShowFloatValue()
+        {
+            value.Text = float.MaxValue.ToString();
+            value1.Text = float.MinValue.ToString();
+            value3.Text = default(float).ToString();
+        }
+
+        private void ShowDoubleValue()
+        {
+            value.Text = double.MaxValue.ToString();
+            value1.Text = double.MinValue.ToString();
+            value3.Text = default(double).ToString();
+        }
+
+        private void ShowDecimalValue()
+        {
+            value.Text = decimal.MaxValue.ToString();
+            value1.Text = decimal.MinValue.ToString();
+            value3.Text = default(decimal).ToString();
+        }
+
+        private void ShowStringValue()
+        {
+            value.Text = "";
+            value1.Text = "";
+            value3.Text = default(string);
+        }
+
+        private void ShowCharValue()
+        {
+            value.Text = "";
+            value1.Text = "";
+            value3.Text = default(char).ToString();
+        }
+
+        private void ShowBoolValue()
+        {
+            value.Text = "";
+            value1.Text = "";
+            value3.Text = default(bool).ToString();
+        }
+
+    }
+}
---
+++ task2_2/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double result = 0;
+            Console.WriteLine("Insert first value:");
+            double v1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Insert operation sign(+,-,*,/,%,^):");
+            string op = Console.ReadLine();
+
+            Console.WriteLine("Insert second value:");
+            double v2 = Convert.ToDouble(Console.ReadLine());
+
+            switch (op)
+            {
+                case "+": result = v1 + v2; break;
+                case "-": result = v1 - v2; break;
+                case "*": result = v1 * v2; break;
+                case "/": result = v1 / v2; break;
+                case "%": result = v1 % v2; break;
+                case "^": result = Math.Pow(v1, v2); break;
+            }
+            Console.WriteLine("Result: {0} {1} {2} = {3}", v1, op, v2, result);
+            Console.ReadKey();
+        }
+    }
+}
